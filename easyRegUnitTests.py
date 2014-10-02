cb0014ffe246590769583688f95d3ba479c11b32
Albinohat <albinohat@gmail.com>
2014-10-02 15:18:28 -0400

    Added the source code and unit test python files.

---
+++ easyRegUnitTests.py
@@ -0,0 +1,24 @@
+## easyRegUnitTests.py
+
+import easyReg
+
+#easyReg.easySetValue("weee", "test", "bork", "weeeeee")
+
+test_key1 = "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\EventLog\Application"
+test_key4 = "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\EventLog\Application\test"
+test_key2 = "HKEY_CURRENT_USER\Console"
+test_key3 = "HKEY_LOCAL_MACHINE\SOFTWARE\FileZilla 3\fzshellext"
+#for each in easyReg.listSubkeys(test_key1):
+	#print each
+
+#print ""
+
+#print easyReg.easyQueryKey(test_key1)
+
+#print easyReg.easyQueryValue(test_key1, "File")
+
+easyReg.easySaveKey(test_key1 + "\\Test", "out.weee")
+
+#easyReg.easyCreateKey(test_key1, "Test")
+
+#easyReg.easyDeleteValue(test_key1, "File")
