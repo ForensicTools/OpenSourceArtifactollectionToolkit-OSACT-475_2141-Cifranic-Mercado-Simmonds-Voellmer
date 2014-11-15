7dc37c37015fbadb625ea207a98cb585dbe43efe
Albinohat <albinohat@gmail.com>
2014-11-15 16:43:31 -0500

    Wrote the test script for the fileHistory module. Fixed a bug in the RecentFile class to account for deleted files which would previously cause a failure.

--- fileHistoryUnitTests.py
+++ fileHistoryUnitTests.py
@@ -15,35 +15,20 @@ if(len(sys.argv) < 2 or len(sys.argv) > 3):
 	print "\n    Invalid syntax! Usage: lasfFile.py *|extension [SID]\n"
 	sys.exit()
 
-## List which will contain registry entries (files).
-list_of_recent_files = []
-
-## Bool enabling debug output.
-bool_debug = 1
-	
-## Connect to the Registry, specifying the hive you wish to connect to.
+## Initialize the registry key object using either the current user or SID specified.
 if (len(sys.argv)  == 2):
 	## Commented line contains key containing last 20 opened/saved files in a dialog.
-	local_key = easyReg.easyOpenKey("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU\\" + sys.argv[1])
+	mru_key = easyReg.RegKey("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU\\" + sys.argv[1])
 	
 else:
 	## Commented line contains key containing last 20 opened/saved files in a dialog.
-	local_key = easyReg.easyOpenKey("HKEY_USERS\\" + sys.argv[2] + "\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU\\" + sys.argv[1])
+	mru_key = easyReg.RegKey("HKEY_USERS\\" + sys.argv[2] + "\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU\\" + sys.argv[1])
 
-## Loop through the values until you run out.
-for i in range(20):
-	try:
-		temp_file = fileHistory.RecentFile(easyReg.RegEntry(_winreg.EnumValue(local_key, i)))
-		if (temp_file.entry.value != "MRUListEx"):
-			list_of_recent_files.append(temp_file)
-		
-	except EnvironmentError:
-		break
-		
-## Sort the list of recent files.
-list_of_recent_files.sort(key=lambda f: int(f.entry.value), reverse=False)
+## Populate the entries of mru_key.	
+mru_key.populateEntries()
 
-## Print the entries.
-if (bool_debug == 1):
-	for file in list_of_recent_files:
-		file.printRecentFile()
+# Print the recent file by looping through the entries of mru_key and initialize RecentFile objects.
+#mru_key.printEntries()
+
+for entry in mru_key.list_of_entries:
+	fileHistory.RecentFile(entry).printRecentFile()
