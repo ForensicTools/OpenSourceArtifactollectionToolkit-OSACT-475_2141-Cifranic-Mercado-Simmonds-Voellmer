b04d506d369205c8d33b98b88ed0c252a4aab8d7
Quantum Paradox <James.QuantumParadox@gmail.com>
2014-12-06 15:19:46 -0500

    Fixed a bug and deleted text files. Moves my code into a folder.

---
+++ James Simmonds/ForensicsViewerUnitTests.py
@@ -0,0 +1,15 @@
+# ForensicsViewerUnitTests.py
+# This prints the results of the commands stated above. 
+# This if statement allows for debugging of ipconfig & whoami is true. 	
+
+import ForensicsViewer
+
+print ForensicsViewer.getIpconfig()
+print ForensicsViewer.getWhoami()
+print ForensicsViewer.getPslist()
+print ForensicsViewer.getPsLoggedon()
+print ForensicsViewer.getTcpvcon()
+print ForensicsViewer.getDiskext()
+print ForensicsViewer.getSmartctl()
+#print ForensicsViewer.getDump()
+
